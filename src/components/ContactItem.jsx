3a57f198247de06e1169e55fa5460eef1a9df26c
Mezzky <ryansahadha@gmail.com>
2023-10-09 23:22:35 +0800

    Add: Delete Button

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,13 +2,15 @@
 import React from 'react'
 import ContactItemBody from './ContactItemBody'
 import ContactItemImage from './ContactItemImage'
+import DeleteButton from './DeleteButton'
 
 // eslint-disable-next-line react/prop-types
-const ContactItem = ({ imageUrl, name, tag }) => {
+const ContactItem = ({ imageUrl, name, tag, onDelete, id }) => {
   return (
     <div className='contact-item'>
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tag} />
+      <DeleteButton onDelete={onDelete} id={id}/>
     </div>
   );
 }
---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+const DeleteButton = ({id, onDelete}) => {
+  return <button className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
+}
+
+export default DeleteButton
