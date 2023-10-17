7a82811a5256a8ff4bf97918606df846c880b878
Rizky Ryan Sahadha <91834119+Mezzky@users.noreply.github.com>
2023-10-17 22:54:30 +0800

    Update DeleteButton.jsx

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 // eslint-disable-next-line react/prop-types
 const DeleteButton = ({id, onDelete}) => {
-  return < b u t ton className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
+  return < b u t t on className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
 }
 
 export default DeleteButton
